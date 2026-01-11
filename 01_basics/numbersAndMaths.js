ecaa018ee3f50bb5fcf6dbd74e85cd45fb640d6f
Afroz Mondal <52920488+afroz2019@users.noreply.github.com>
2026-01-11 16:39:46 +0000

    exploring number and math

---
+++ 01_basics/numbersAndMaths.js
@@ -0,0 +1,57 @@
+const price = 200;
+// console.log(price);
+// console.log(typeof price);
+
+const newPrice = new Number(200);
+// console.log(newPrice);
+// console.log(typeof newPrice);
+
+const priceStr = newPrice.toString();
+
+// console.log(priceStr);
+// console.log(typeof priceStr);
+// console.log(priceStr.length);
+
+// console.log(newPrice.toFixed(1));
+// console.log(newPrice.toFixed(2));
+
+const otherNumber = 324.486903;
+// console.log(otherNumber.toPrecision(5));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+//+++++++++++++++ Maths +++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.2));
+
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4, 5,1,7,9));
+// console.log(Math.max(4, 5,1,7,9));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 1000;
+const max = 9999;
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
